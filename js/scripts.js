59ea0bb79d46e5865df2b31fd212c2f0bf1efb62
Andrew Niekamp and Rick Thornbrugh <andrew+rick@>
2016-06-22 13:52:23 -0700

    add single letter consonant comparison for input

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,20 @@
 var inputArray = [];
 var translation = '';
 var input = '';
-var vowels = ['a', 'e', 'i', 'o', 'u'];
+var vowels = ['a','e','i','o','u'];
 
 var translate = function(words) {
   inputArray = words.split("");
+  // for (var i = 0; i < vowels.length; i++) {
+    if (inputArray[0] !== vowels[0] &&
+        inputArray[0] !== vowels[1] &&
+        inputArray[0] !== vowels[2] &&
+        inputArray[0] !== vowels[3] &&
+        inputArray[0] !== vowels[4]) {
+      var consonant = inputArray.shift();
+      inputArray.push(consonant);
+    }
+  // }
   inputArray.push("ay");
   translation = inputArray.join("");
 };
