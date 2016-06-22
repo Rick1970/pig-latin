2da4fca966e218f502269292760844bcd8a1761c
Andrew Niekamp and Rick Thornbrugh <andrew+rick@>
2016-06-22 13:30:16 -0700

    seperate out business logic, from user interface, and created vowel array

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@
 
 var inputArray = [];
 var translation = '';
+var input = '';
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+var translate = function(words) {
+  inputArray = words.split("");
+  inputArray.push("ay");
+  translation = inputArray.join("");
+};
 
 
 // jQuery user interface logic here.
@@ -9,8 +17,9 @@ $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
     var input = $("input#words").val();
-    inputArray = input.split("");
-    inputArray.push("ay");
-    translation = inputArray.join("");
+
+    translate(input);
+
+    console.log(translation);
   });
 });
