f3899fb81f2b0d162d592a5fe1574595a286ed21
Andrew Niekamp and Rick Thornbrugh <andrew+rick@>
2016-06-22 14:35:45 -0700

    complete consonant comparison function

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,19 @@ var vowels = ['a','e','i','o','u'];
 
 var translate = function(words) {
   inputArray = words.split("");
-  // for (var i = 0; i < vowels.length; i++) {
+  for (var i = 0; i < inputArray.length; i++) {
     if (inputArray[0] !== vowels[0] &&
         inputArray[0] !== vowels[1] &&
         inputArray[0] !== vowels[2] &&
         inputArray[0] !== vowels[3] &&
         inputArray[0] !== vowels[4]) {
-      var consonant = inputArray.shift();
+      var consonant = inputArray[0];
       inputArray.push(consonant);
+      inputArray.shift();
+    } else {
+      break;
     }
-  // }
+  }
   inputArray.push("ay");
   translation = inputArray.join("");
 };
